752fbe71e1f4087f2b7308bbdf317cc8f515e85c
harimChoi <holly001503@gmail.com>
2022-01-14 09:42:58 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Algorithm
+# https://teamsparta.notion.site/Chapter-2-W2-W5-740098f7060548d0a9c40703f915875c
+
+
+def ch4(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    #bigO, 자료구조
+
+    #0114 20pm 강의 ch5, 6 문자열, 7 배열
+    ##개념
+    ##문제
+    ##과제
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    ch4('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
